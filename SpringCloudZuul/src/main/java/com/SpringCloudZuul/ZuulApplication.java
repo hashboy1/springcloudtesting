5cb5f99f4b88e6ca30d9300cafd84601361c15b5
username <email>
2018-09-26 15:41:45 +0800

    20180926

--- SpringCloudZuul/src/main/java/com/SpringCloudZuul/ZuulApplication.java
+++ SpringCloudZuul/src/main/java/com/SpringCloudZuul/ZuulApplication.java
@@ -2,16 +2,27 @@ package com.SpringCloudZuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableDiscoveryClient    //if you need get appid from eureka, this annotation will be necessary
 public class ZuulApplication {
 
       public static void main(String[] args) throws Exception {
 
            SpringApplication.run(ZuulApplication.class, args);
-
+           
+           
+/*  you can add belows contents to configuration file. if not, all services on ereka will be added to zuul gateway.
+*   you can access the service by zuulip+port/applicationID/**
+*   but please attention to the applicationid will change to lower case.    
+zuul:
+  routes:
+    SERVICEAPP1: /SERVICEAPP1/**	
+    
+            */
       }
 
 }
